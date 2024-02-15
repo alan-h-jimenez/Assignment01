942d099d0e76ceb3a0191ac03fc59b057373625f
Alan Jimenez Cervantes <uc.mail.box@gmail.com>
2024-02-14 23:53:35 -0800

    Assignment 1 Answers

---
+++ js/script.js
@@ -0,0 +1,145 @@
+// STEP 1
+
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+// STEP 2
+
+// 3.1416      // numeric literal expression
+// 'Is it?'    // string literal expression
+// true        // Boolean literal expression
+// null        // null literal expression
+
+// STEP 3
+
+// let complexVariableExpression1 = (97 * pseudoRandomNumber0 + 43) % 100
+// let complexVariableExpression2 = parseInt(userBasketContent) > 0
+
+// STEP 4
+
+// let strFirstName
+// let strLastName
+// let strAddress
+// let strCity
+// let strState
+// let strZipCode
+// let numYourAge
+// let strReferralSource
+// let strMayWeContactYou
+
+// STEP 5
+
+// let strState = prompt('State')
+// strState = strState.toUpperCase()
+// let strReferralSource = 'website'
+// let strMayWeContactYou = confirm('May We Contact You?')
+
+// STEP 6
+
+// let result = 55 + ' years'
+// console.log(result)
+
+// STEP 7
+
+// let result1 = true + ' facts'
+// console.log(result1)
+
+// let result2 = true + 12
+// console.log(result2)
+
+// STEP 8
+
+/*
+The string literal is not a valid expresion. 
+The single quote between I and m (I'm) is treated as the end of the string literal.
+The browser assumes that "m" is an identifier.
+*/
+
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+// console.log(someString)
+
+// STEP 9
+
+// let value1 = null
+// console.log(value1)
+
+// let value2
+// console.log(value2)
+
+// STEP 10
+
+// console.log(typeof 'This might be a string')
+// console.log(typeof 3.1416)
+// console.log(typeof ('String' === 'string'))
+// console.log(typeof window)
+// console.log(typeof x)
+
+// STEP 11
+
+// alert('Hello ' + 'Alan Jimenez Cervantes' + ', welcome to the JavaScript class!')
+
+// STEP 12
+
+// let name = 'Alan Jimenez Cervantes'
+// alert('Hello ' + name + ', welcome to the JavaScript class!')
+
+// STEP 13
+
+// let name = 'Alan Jimenez Cervantes'
+// let course = 'JavaScript'
+// alert('Hello ' + name + ', welcome to the ' + course + ' class!')
+
+// STEP 14
+
+// let name = 'Alan Jimenez Cervantes'
+// let course = 'JavaScript'
+// alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!')
+
+// STEP 15
+
+// let name = prompt('What is your name?')
+// let course = 'JavaScript'
+// alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!')
+
+// STEP 16
+
+// let name = prompt('What is your name?')
+// let course = prompt('What class are you taking?')
+// alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!')
+
+// STEP 17
+
+// let x = 10
+// let y = 20
+// console.log(x + y)
+
+// STEP 18
+
+// let x = 20
+// x += 20
+// console.log(x)
+
+// STEP 19
+
+// let x = 20
+// x *= 5
+// console.log(x)
+
+// STEP 20
+
+// let x = 20 % 3
+// x /= 1
+// console.log(x)
+
+// STEP 21
+
+// let x = 10
+// console.log(x > 100 || x % 2 == 0)
+
+// STEP 22
+
+// let name = 'pokemon'
+// console.log(name.length > 5 && name.substring(0, 1) == name.substring(0, 1).toUpperCase())
